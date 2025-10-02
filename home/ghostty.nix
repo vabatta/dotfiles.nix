4dc684d2bed2ec321645eee0ba44afa3b16512d6
vabatta <2137077+vabatta@users.noreply.github.com>
2025-10-02 23:46:54 +0200

    fix ghostty colors

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -9,8 +9,10 @@
       theme = "light:Catppuccin Latte,dark:Catppuccin Mocha";
       font-family = "Hack Nerd Font";
       macos-option-as-alt = true;
+      macos-titlebar-style = "native";
       confirm-close-surface = false;
       mouse-hide-while-typing = true;
+      cursor-color = "cell-foreground";
     };
   };
 }
